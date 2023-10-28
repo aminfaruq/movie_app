20050d4fbb5aa340e8e3504ff0b07a85c2fd93e7
Amin Faruq <aminfaruk.fa@gmail.com>
2023-10-28 12:19:09 +0800

    init flutter

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MovieApp());
+}
+
+class MovieApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("TMDB API"),
+      ),
+      body: Container(),
+    );
+  }
+}
